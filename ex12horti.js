8cd31ecfdd9189886382cb67e5c1bd3f53a50e12
lizandraevani <lizandra.nascimento@discentes.fat.uerj.br>
2024-09-11 21:26:48 -0300

    Add files via upload

---
+++ ex12horti.js
@@ -0,0 +1,15 @@
+fruta = prompt("informe a fruta")
+
+switch (fruta) {
+    case "maça":
+        console.log("nao temos essa fruta aqui");
+        break
+        case "kiwi":
+            console.log("estamos com escassez de kiwis")
+            break
+            case "melancia":
+            console.log("aqui esta, são 3 reais o quilo")
+            break
+            default:
+                console.log("erro")
+}
---
+++ ex13carro.js
@@ -0,0 +1,18 @@
+carro = prompt("informe o modelo do carro")
+
+switch (carro) {
+    case "hatch":
+        console.log("compra efetuada com sucesso");
+        break
+        case "sedans":
+            console.log("tem certeza que nao prefere este modelo?")
+            break
+        case "motocicletas":
+            console.log("tem certeza que nao prefere este modelo?")
+            break
+        case "caminhonetes":
+                console.log("tem certeza que nao prefere este modelo?")
+                break
+        default:
+                console.log("nao trabalhamos com este tipo e automovel aqui")
+}
